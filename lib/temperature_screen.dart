5573c1d690783daf4cec464c4133d32d001904e4
Nyanzu-Kokayne <nyanzubuah@gmail.com>
2026-07-16 09:00:52 +0100

    Bugs and error fixed in temperature app

--- lib/temperature_screen.dart
+++ lib/temperature_screen.dart
@@ -150,6 +150,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
 
                 SizedBox(height: 50),
+                
                 Container(
                   width: double.infinity,
                   //alignment: Alignment(0, 0),
